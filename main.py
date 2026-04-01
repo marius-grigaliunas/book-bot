c841fc26a9ac50d57c0a9efbcb195825ca3848dc
marius-grigaliunas <cia.marius.grigaliunas@gmail.com>
2026-04-01 20:33:38 +0200

    simple text analysis functions to get text and word count

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file:
+        file_contents = file.read()
+
+    return(file_contents)
+
+def get_word_count(book_text: str):
+    split_text = book_text.split()
+
+    return len(split_text)
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    word_count = get_word_count(book_text)
+
+    print(f"Found {word_count} total words")
+
+main()
