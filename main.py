6fdfec82edf52c7af615f83c2cbd5fe3375e68b8
marius-grigaliunas <cia.marius.grigaliunas@gmail.com>
2026-04-04 07:33:53 +0200

    added dict to list of dicts method, more books, formatting

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from stats import get_book_text, get_word_count, get_char_usage
+import sys
+from stats import get_book_text, get_word_count, get_char_usage, sorted_list_from_dict
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1] 
+    text = get_book_text(filepath)
     word_count = get_word_count(text)
     characters = get_char_usage(text)
 
+    sorted_characters = sorted_list_from_dict(characters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
-    print(characters)
+    print("--------- Character Count -------")
+    
+    for item in sorted_characters:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
+
 main()
--- stats.py
+++ stats.py
@@ -17,3 +17,19 @@ def get_char_usage(text: str):
             char_dict[letter]+=1
     
     return char_dict
+
+def sorted_list_from_dict(letter_dict: dict):
+    new_list = []
+
+    for letter, frequency in letter_dict.items():
+        new_list.append({
+            "char": letter,
+            "num": frequency
+        })
+    
+    def sort_on_frequency(dict_list: list):
+        return dict_list["num"]
+
+    new_list.sort(reverse=True, key=sort_on_frequency)
+
+    return new_list
